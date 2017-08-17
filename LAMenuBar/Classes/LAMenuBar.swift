c1c24a6c72928287c3a86f880ede019f7a039b20
Luis Arias <lugearma@gmail.com>
2017-08-17 00:03:56 -0500

    Create new file to put model

--- LAMenuBar/Classes/LAMenuBar.swift
+++ LAMenuBar/Classes/LAMenuBar.swift
@@ -13,27 +13,6 @@ protocol LAMenuBarDelegate: class {
   func didSelectItemAt(indexPath: Int)
 }
 
-public struct LAMenuModel {
-  
-  public let images: [UIImage?]
-  public let backgroundColor: UIColor
-  public let barColor: UIColor
-  public let tintColorWhenSelected: UIColor
-  public let tintColorWhenDiselected: UIColor
-  public let sections: Int
-  public let views: [UIView]
-  
-  public init(images: [UIImage?], backgroundColor: UIColor, barColor: UIColor, tintColorWhenSelected: UIColor, tintColorWhenDiselected: UIColor, views: [UIView]) {
-    self.images = images
-    self.backgroundColor = backgroundColor
-    self.barColor = barColor
-    self.tintColorWhenSelected = tintColorWhenSelected
-    self.tintColorWhenDiselected = tintColorWhenDiselected
-    self.sections = views.count
-    self.views = views
-  }
-}
-
 @available(iOS 9.0, *)
 public final class LAMenuBar: UIView {
   
